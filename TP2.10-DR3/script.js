6ed9d1c66ec4f470fa2e9e97554d41ae17d77720
Lohran Mendes <lohranfm@gmail.com>
2025-03-13 15:22:59 -0300

    chore: question 10 solved

--- TP2.10-DR3/script.js
+++ TP2.10-DR3/script.js
@@ -4,3 +4,29 @@
 // Requisitos:
 // Ao passar o mouse, aplique um efeito CSS (como girar, mover ou redimensionar).
 // Ao clicar, mude a cor do quadrado e exiba um texto abaixo dele.
+const quadrado = document.getElementById("quadrado");
+const body = document.querySelector("body");
+const paragrafo = document.createElement("p");
+body.appendChild(paragrafo);
+let timeoutID;
+
+quadrado.addEventListener("click", () => {
+  clearTimeout(timeoutID);
+  if (paragrafo.innerText.trim().length === 0) {
+    paragrafo.innerText = "Quadrado clicado 1 vez!";
+  } else {
+    const texto = paragrafo.innerText.split(" ");
+    paragrafo.innerText = `Quadrado clicado ${Number(texto[2]) + 1} vezes!`;
+  }
+  timeoutID = setTimeout(() => (paragrafo.innerText = ""), 5000);
+  quadrado.style.backgroundColor === "blue"
+    ? (quadrado.style.backgroundColor = "yellow")
+    : (quadrado.style.backgroundColor = "blue");
+});
+
+quadrado.addEventListener("mouseenter", () => {
+  quadrado.classList.add("girar");
+});
+quadrado.addEventListener("mouseleave", () => {
+  quadrado.classList.remove("girar");
+});
